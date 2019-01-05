784372f74c6223fa42a31ba4a16d3f947eb76c22
srnitish <sr.nitish06@gmail.com>
2019-01-05 23:21:35 +0530

    Last changes on 5-01-2019

--- src/app/shared/customer.service.ts
+++ src/app/shared/customer.service.ts
@@ -7,14 +7,10 @@ import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
   providedIn: 'root'
 })
 
-
 export class CustomerService {
 
   constructor(private firebase: AngularFireDatabase) { }
   customerList: AngularFireList<any>;
-
-
-
   //declare of the variable type formGroup as from
   //Inside FormGroup Instance, we have to provide objects, containing properties of customers.
   form = new FormGroup({
